4da4afe8393682aa3421851db8daf4038b356000
Lucas Cecchi <lucascecchi@gmail.com>
2021-11-13 14:48:52 -0500

    Removed output parsing, opting for dice coefficients only.

--- main_baseline.py
+++ main_baseline.py
@@ -4,18 +4,11 @@ from dataset import AnswersCSVDataset, q_rubric_dict
 import subprocess
 
 
-def TextSimilarity(str1, str2):
+def text_similarity(str1, str2):
     cmd = "perl Text-Similarity-0.13/bin/text_similarity.pl " + "--string " + "'" + str1 + "' '" + str2 + "'" + \
-          " --type Text::Similarity::Overlaps" + " --normalize" + " --verbose"
+          " --type Text::Similarity::Overlaps" + " --normalize"
     out = subprocess.getoutput(cmd=cmd)
-    results = {}
-
-    for line in out.splitlines():
-        if ':' in line:
-            l = line.split(':')
-            results[l[0].rstrip().lstrip()] = float(l[1].rstrip().lstrip())
-
-    return results
+    return float(out) # Dice Coefficient
 
 
 parser = argparse.ArgumentParser()
@@ -27,7 +20,6 @@ parser.add_argument('--answer_data_path', type=str, default="./answers_train.csv
 
 args = parser.parse_args()
 
-#
 print("Loading Answer Data")
 answers_datset = AnswersCSVDataset([args.answer_data_path])
 print("Done")
@@ -43,10 +35,8 @@ for row in tqdm(answers_datset):
     # Get question text --> question_line
     question_reference = q_rubric_dict[q_id]
 
-    for a in question_reference:
-        r = TextSimilarity(answer_line, a)
-        print(r)
-    print(r)
+    dice_coefficients = [text_similarity(answer_line, a) for a in question_reference]
 
-    # Do some inference on r --> using Dice, Cosine, Lesk, or other measures
-    # And predict label
+    avg_dice = sum(dice_coefficients) / len(dice_coefficients)
+
+    # Do some inference on avg_dice
