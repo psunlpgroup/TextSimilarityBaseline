d220ad13ef8e6af1bfda2c7e058e15c00f0c9349
Lucas Cecchi <lucascecchi@gmail.com>
2021-11-13 17:23:15 -0500

    Added comments for text_similarity function

--- main_baseline.py
+++ main_baseline.py
@@ -6,34 +6,34 @@ import re
 
 
 def text_similarity(str1, str2):
+    """
+    Wrapper method for TextSimilarity perl scripts
+    :param str1: String to compare to str2
+    :param str2: String to compare to str1
+    :return: Dices coefficient of the two token sequences
+    """
     cmd = "perl Text-Similarity-0.13/bin/text_similarity.pl " + "--string " + "'" + str1 + "' '" + str2 + "'" + \
           " --type Text::Similarity::Overlaps" + " --normalize"
-
     out = subprocess.run(cmd, shell=True, capture_output=True, text=True)
-    if out.stderr:
-        print(cmd)
-        print(out.stderr)
-        exit(0)
-    return float(out.stdout)  # Dice Coefficient
+    return float(out.stdout)
 
 
+# Arguments:
 parser = argparse.ArgumentParser()
-
-# Data args
-parser.add_argument('--answer_data_path', type=str, default="./answers_train.csv",
+parser.add_argument('--ata_path', type=str, default="./answers_train.csv",
                     help='Path to the input answer dataset')
 parser.add_argument('--correct_threshold', type=float, default=.5)
 parser.add_argument('--partial_threshold', type=float, default=.2)
 
 args = parser.parse_args()
 
-print("Loading Answer Data")
-answers_datset = AnswersCSVDataset([args.answer_data_path])
-print("Done")
+# Load Data
+csv_dataset = AnswersCSVDataset([args.data_path])
+
 
 total = 0
 accurate = 0
-for row in tqdm(answers_datset):
+for row in tqdm(csv_dataset):
     a_id = row[0]  # answer id
     answer_line = row[1].strip()  # answer text
     q_id = row[2]  # question id
@@ -43,6 +43,7 @@ for row in tqdm(answers_datset):
     # Get question text --> question_line
     question_reference = q_rubric_dict[q_id]
 
+    # Clean answer text
     answer_line = re.sub(r'[^a-zA-Z0-9]', '', answer_line)
 
     dice_coefficients = [text_similarity(answer_line, re.sub(r'[^a-zA-Z0-9]', '', q))
@@ -55,7 +56,6 @@ for row in tqdm(answers_datset):
     accurate += (a_score == 'correct' and correct_pred) or \
                 (a_score == 'partial correct' and partial_pred) or \
                 (a_score == 'incorrect' and not correct_pred)
-
     total += 1
 
 # Calculate Accuracy
